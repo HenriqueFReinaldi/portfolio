9ad8d9d321e5f762e344ca6dea5b52eee684bdff
Henrique Reinaldi <henriqreinaldi@gmail.com>
2026-03-30 15:11:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ function updateIntro(){
     const d = new Date()-new Date('2007-01-13');
     const anos = Math.floor(d/31557600000);
 
-    document.getElementById("intro").textContent = `Tenho ${anos} anos. Sou aluno de ciências da computação no Centro Universitário de Brasília.`
+    document.getElementById("intro").textContent = `Tenho ${anos} anos. Sou um aluno de ciência da computação no Centro Universitário de Brasília.`
 }
-
 
 updateIntro();
