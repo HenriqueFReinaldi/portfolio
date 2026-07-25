24ccef7b10692529a7300320789f3cd4e506f268
Henrique Reinaldi <henriqreinaldi@gmail.com>
2026-07-25 02:54:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,5 +4,27 @@ function updateIntro(){
 
     document.getElementById("intro").textContent = `Tenho ${anos} anos. Sou um aluno de ciência da computação no Centro Universitário de Brasília.`
 }
-
 updateIntro();
+
+
+const vo = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const video = entry.target;
+        
+        if (entry.isIntersecting) {
+            video.play().catch(err => console.log("Playback prevented:", err));
+        } 
+        else {
+            video.pause();
+        }
+    });
+
+}, 
+{ 
+  threshold: 0.5
+}
+);
+
+document.querySelectorAll('video').forEach(v => {
+    vo.observe(v);
+});
